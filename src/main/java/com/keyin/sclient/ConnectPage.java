aeca78ef2cc660a5232f505f0347d0198e749c56
Riley <riley.ledrew@keyin.com>
2023-10-31 13:43:37 -0230

    Initialized the UI
    Figured out how to get the display working, as well as button interactions and text fields have basic test functions. Next up is adding memory storage for inputted text and then routing for the server. Then a new page will need to be created for interacting with the server properly.

---
+++ src/main/java/com/keyin/sclient/ConnectPage.java
@@ -0,0 +1,57 @@
+package com.keyin.sclient;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ConnectPage implements ActionListener {
+    JFrame frame = new JFrame("Client");
+    JTextField IP = new JTextField();
+    JTextField port = new JTextField();
+    JButton connectButton = new JButton("Connect");
+
+    public ConnectPage() {
+
+
+
+
+        // Create Connection Panel
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(new JLabel("Welcome to the Client!"));
+        panel.add(new JLabel("Please enter the IP address and port of the server you wish to connect to."));
+        panel.add(new JLabel("IP Address:"));
+        panel.add(IP);
+        panel.add(new JLabel("Port:"));
+        panel.add(port);
+        panel.add(connectButton);
+        connectButton.addActionListener(this);
+        connectButton.setFocusable(false);
+
+
+
+
+        //Configure Frame
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Main Menu");
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setSize(420,420);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == connectButton) {
+            System.out.println("Hello");
+        }
+
+    }
+
+
+}
